5eb7339fadce9190a2a626e0029708835b73b850
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-11-04 15:08:22 +0900

    依存記述の更新

--- setup.py
+++ setup.py
@@ -8,10 +8,6 @@ entries = list(parse_requirements(path, session=False))
 install_reqs = [str(entry.req) for entry in entries if entry.req]
 dep_links = [str(entry.link) for entry in entries if entry.link]
 
-print(install_reqs)
-print(dep_links)
-
-exit()
 
 setup(
     name="sugo-unit-dummy",
