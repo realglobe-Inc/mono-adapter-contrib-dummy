e313dbef83e839a1566b820a1a48f028494fb59b
Fukuchi Daisuke <fukuchidaisuke@realglobe.jp>
2015-11-04 11:53:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ setup(
     name="sugo-unit-dummy",
     version="0.1",
     packages=["sugo.unit"],
-    install_requires=[
-        "sugounitlib",
-    ],
+    install_requires=["sugounitlib"],
     dependency_links=["http://github.com/realglobe-Inc/sugo-unit-lib/tarball/master#egg=sugounitlib"],
 )
