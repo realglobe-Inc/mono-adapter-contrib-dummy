5322b2c27825f8336383c0dc9016b9ed68db23cb
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-11-04 15:25:37 +0900

    依存記述の更新

--- setup.py
+++ setup.py
@@ -5,8 +5,28 @@ from setuptools import setup
 
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "requirements.txt")
 entries = list(parse_requirements(path, session=False))
-install_reqs = [str(entry.req) for entry in entries if entry.req]
-dep_links = [str(entry.link) for entry in entries if entry.link]
+install_reqs = []
+dep_links = []
+
+for entry in entries:
+    if not entry.req:
+        continue
+    if not entry.link:
+        install_reqs.append(str(entry.req))
+        continue
+
+    if not (entry.link.netloc == "github.com"):
+        install_reqs.append(str(entry.req))
+        dep_links.append(str(entry.link))
+        continue
+
+    if entry.link.egg_fragment.find("-") < 0:
+        install_reqs.append("{0}==master".format(str(entry.req)))
+        dep_links.append("{0}-master".format(str(entry.link)))
+        continue
+
+    install_reqs.append(str(entry.req)[::-1].replace("-", "==", 1)[::-1])
+    dep_links.append(str(entry.link))
 
 
 setup(
