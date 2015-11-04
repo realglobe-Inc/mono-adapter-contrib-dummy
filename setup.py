127bda157c477a83866ef8512228431fbc2596f2
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-11-04 13:33:18 +0900

    依存記述の更新

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+from pip.req import parse_requirements
+import os
 from setuptools import setup
+
+
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "requirements.txt")
+install_reqs = [str(req.req) for req in parse_requirements(path, session=False)]
+
 
 setup(
     name="sugo-unit-dummy",
     version="0.1",
     packages=["sugo.unit"],
-    install_requires=["sugo.unit.base"],
-    dependency_links=["git+https://github.com/realglobe-Inc/sugo-unit-base#egg=sugo.unit.base"],
+    install_requires=install_reqs,
 )
