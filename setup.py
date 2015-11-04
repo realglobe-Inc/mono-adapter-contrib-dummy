6df675f1bd0f4698d04179a81ec64b8825398f01
Fukuchi Daisuke <fukuchidaisuke@realglobe.jp>
2015-11-04 12:01:36 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="sugo-unit-dummy",
     version="0.1",
     packages=["sugo.unit"],
-    install_requires=["sugo-unit-lib"],
-    dependency_links=["git+https://github.com/realglobe-Inc/sugo-unit-lib#egg=sugo_unit_lib"],
+    install_requires=["sugo.unit.base"],
+    dependency_links=["git+https://github.com/realglobe-Inc/sugo-unit-base#egg=sugo.unit.base"],
 )
